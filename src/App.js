79024718545508e23c4d4822134a2893cdfba631
Danny Michael Jaramillo Jumbo <52290950+DannyJaramil@users.noreply.github.com>
2022-02-09 15:51:08 -0500

    listo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
     event.preventDefault();
   }
-  "proxy": "",
+  
   const PeticionPut = async (props) => {
     let anio = document.getElementById('anio').value
     let mes = document.getElementById('mes').value
